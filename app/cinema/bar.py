adabd9c2b17147ea2d0a50ecca65d0c5f3c54352
madlings <nagalingam.s@gmail.com>
2026-02-11 17:53:34 +0530

    Update bar.py

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -4,8 +4,5 @@ from app.people.customer import Customer
 class CinemaBar:
 
     @staticmethod
-    def sell_product(product: str, customer: Customer | str) -> None:
-        if isinstance(customer, Customer):
-            print(f"Cinema bar sold {product} to {customer.name}.")
-        else:
-            print(f"Cinema bar sold {product} to {customer}.")
+    def sell_product(product: str, customer: Customer) -> None:
+        print(f"Cinema bar sold {product} to {customer.name}.")
